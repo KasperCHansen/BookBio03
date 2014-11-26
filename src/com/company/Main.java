72b4e8da159757562a3ea5e45c8535cb2c3edf6a
Kasper Christian Hansen <kasperchansen@hotmail.com>
2014-11-26 12:07:44 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+    private static int test = 200;
+    private static int test2 = 200;
+
+    public static void main(String[] args) {
+	if(test == test2){
+            System.out.println("YOU MADE IT");
+        }
+
+        TEST TEST1 = new TEST();
+    }
+
+    public void helloWorld() {
+
+
+        System.out.println("LOLOLOLOL");
+    }
+}
---
+++ src/com/company/TEST.java
@@ -0,0 +1,11 @@
+package com.company;
+
+/**
+ * Created by kasperhansen on 26/11/14.
+ */
+public class TEST {
+
+    public TEST() {
+        System.out.print("new object");
+    }
+}
